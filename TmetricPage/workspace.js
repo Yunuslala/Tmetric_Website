ac1fbd30355ea49d7f5c33b2725e84d8b8b95444
Yunuslala <saifjava2@gmail.com>
2022-11-12 00:19:09 +0530

    completed Day-4

--- TmetricPage/workspace.js
+++ TmetricPage/workspace.js
@@ -6,3 +6,6 @@ document.querySelector("#Space").addEventListener("click",function(){
     window.location="createWork.html"
     console.log("space")
 })
+
+fetch(`C:\Masai_Projects\images.json`).then((res)=>res.json()).then((data)=>{
+    console.log(data)})
